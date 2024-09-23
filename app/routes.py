6d6d1d3387831718e894310561283d5dba8425cd
WiktoriaWezgraj <wiktoriawezgraj1@gmail.com>
2024-09-23 15:53:32 +0200

    python code improved

--- app/routes.py
+++ app/routes.py
@@ -1,75 +1,68 @@
 '''Lista wydatków, aktualny stan konta z uwzg. wpływow, wypłat'''
 
-from flask import Flask, render_template, request, redirect, url_for, jsonify
 import json
-
-app = Flask(__name__)
 
 class Finance():
     def __init__(self):
         self.filepath = "tracker.json"
 
-    def deposit(self, amount):
+    def deposit(self):
+        amount = input("How much you want to deposit?")
         try:
             with open(self.filepath, "r") as f:
                 data = json.load(f)
             data["balance"] += amount
             print(data)
-            json.dump(data, open(self.filepath, 'w'))
-            return data
-        # make deposit with balance, later create app with nice format and graphs with matplotlib 
+            with open(self.filepath, 'w') as f:
+                json.dump(data, f)
+            
         except ValueError:
-            print("Invalid input.")
+            print("Invalid input")
         except FileNotFoundError:
-            print("Tracker not found.")
+            print("Tracker not found")
 
     def create(self):
-        # create new dictionary for new month
         my_dict = {}
-        categories = ["balance", "grocerries", "transport", "fees", "entertainment", "clothes", "restaurant"]
+        categories = ["balance", "groceries", "transport", "fees", "entertainment", "clothes", "restaurant"]
         values = [0, 0, 0, 0, 0, 0, 0]
 
-        # display dictionary and save to json
         for i in range(len(categories)):
             my_dict[categories[i]] = values[i]
+
         print(my_dict)
-        json.dump(my_dict, open(self.filepath, 'w'))
+        with open(self.filepath, 'w') as f:
+            json.dump(my_dict, f)
         
-    def payment(self, category, f_amount):
+    def payment(self):
+        try:
+            with open(self.filepath, "r") as f:
+                data = json.load(f)
 
-        with open(self.filepath, "r") as f:
-            data = json.load(f)
+            print("Categories:")
+            for option in data.keys():
+                print(option)
+            category = input("What is your choice?")
+            
+            f_amount = input("How much did you pay?")
+            f_amount= f_amount.replace(",", ".")
+            f_amount = float(f_amount)
 
-        if category in data:
-            data[category] += f_amount
-            data["balance"] -= f_amount
-            json.dump(data, open(self.filepath, 'w'))
-            return data
-        else:
-            return "Invalid category."              
-                
+            if category in data.keys():
+                data[category] += f_amount
+                data["balance"] -= f_amount
+            
+                with open(self.filepath, 'w') as f:
+                    json.dump(data, f)
+            else:
+                print("Invalid category")
+            print(data)
+            
+        except FileNotFoundError:
+            print("Tracker not found")
+        except ValueError:
+            print("Invalid amount")
+
 f = Finance()
+f.payment()
 
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/create', methods=['POST'])
-def create_tracker():
-    data = f.create()
-    return jsonify(data)
-
-@app.route('/deposit', methods=['POST'])
-def deposit():
-    amount = float(request.form['amount'])
-    data = f.deposit(amount)
-    return jsonify(data)
-
-@app.route('/payment', methods=['POST'])
-def make_payment():
-    category = request.form['category']
-    f_amount = float(request.form['amount'])
-    data = f.payment(category, f_amount)
-    return jsonify(data)
 
